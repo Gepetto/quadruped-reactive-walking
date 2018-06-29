5bf48bcdc33165309a8f57e6017930000aca0c4f
Guilhem Saurel <guilhem.saurel@laas.fr>
2018-06-29 19:25:29 +0200

    [Tests][Python] Add minimalistic python test

---
+++ tests/python/test_add.py
@@ -0,0 +1,13 @@
+import unittest
+
+import example_adder as exa
+
+
+class TestAdder(unittest.TestCase):
+    def test_adder_integers(self):
+        self.assertEqual(exa.add(4, 3), 7)
+        self.assertEqual(exa.sub(4, 3), 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
