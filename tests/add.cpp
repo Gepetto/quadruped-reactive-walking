6726bf0980b86e5420e35c09c7805455b1c1daef
Guilhem Saurel <guilhem.saurel@laas.fr>
2018-06-29 18:25:38 +0200

    [Tests] Add minimalist test

---
+++ tests/add.cpp
@@ -0,0 +1,9 @@
+#include <cassert>
+#include "example-adder/gepadd.hpp"
+
+int main() {
+  assert(gepetto::example::add(1, 2) == 3);
+  assert(gepetto::example::add(5, -1) == 4);
+  assert(gepetto::example::add(-3, -1) == -4);
+  return 0;
+}
