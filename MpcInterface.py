f6698b82ae108a4e30b2bc4994f9f4000ecbe028
Pierre-Alexandre Leziart <paleziart@laas.fr>
2020-02-28 18:45:02 +0100

    Continuing MPC Interface

--- MpcInterface.py
+++ MpcInterface.py
@@ -10,9 +10,13 @@ class MpcInterface:
 
     def __init__(self):
 
-        self.oMs = pin.SE3.Identity()
+        self.qmes12 = np.zeros((19, 1))
+        self.vmes12 = np.zeros((18, 1))
 
-    def update(self):
+        self.oMb = pin.SE3.Identity()
+        self.oMl = pin.SE3.Identity()
+
+    def update(self, pyb_sim, solo):
 
         ###############################
         # Retrieve data from PyBullet #
@@ -23,10 +27,32 @@ class MpcInterface:
         baseVel = pyb.getBaseVelocity(pyb_sim.robotId)  # Velocity of the trunk
 
         # Joints configuration and velocity vector for free-flyer + 12 actuators
-        qmes12 = np.vstack((np.array([baseState[0]]).T, np.array([baseState[1]]).T,
-                            np.array([[jointStates[i_joint][0] for i_joint in range(len(jointStates))]]).T))
-        vmes12 = np.vstack((np.array([baseVel[0]]).T, np.array([baseVel[1]]).T,
-                            np.array([[jointStates[i_joint][1] for i_joint in range(len(jointStates))]]).T))
+        self.qmes12 = np.vstack((np.array([baseState[0]]).T, np.array([baseState[1]]).T,
+                                 np.array([[jointStates[i_joint][0] for i_joint in range(len(jointStates))]]).T))
+        self.vmes12 = np.vstack((np.array([baseVel[0]]).T, np.array([baseVel[1]]).T,
+                                 np.array([[jointStates[i_joint][1] for i_joint in range(len(jointStates))]]).T))
+
+        ################
+        # Process data #
+        ################
+
+        # Get SE3 object from world frame to base frame
+        self.oMb = pin.SE3(self.qmes12[3:7], self.qmes12[0:3])
+        self.RPY = pin.matrixToRpy(self.oMb.rotation)
+
+        # Get SE3 object from world frame to local frame
+        self.oMl = pin.SE3(pin.utils.rotate('z', self.RPY[2]), np.array([self.qmes12[0], self.qmes12[1], 0.0]))
+
+        # Get center of mass from Pinocchio
+        pin.centerOfMass(solo.model, solo.data, self.qmes12, self.vmes12)
+
+        # Store result
+        oC = solo.data.com[0]
+        oV = solo.data.vcom[0]
+
+        # Get position and velocity in local frame
+        lC = oMl.inverse() * oC
+        lV = oMl.rotation.transpose() @ oV
 
         return 0
 
