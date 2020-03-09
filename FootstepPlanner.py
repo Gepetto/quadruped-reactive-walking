Trying to fix rotation issue + Feet trajectory generator now using PyBullet vel and acc for feet

FootstepPlanner.py
@@ -94,7 +94,7 @@ class FootstepPlanner:
         # Add velocity forecast
         #  p += np.tile(v[0:2, 0:1], (1, 4)) * t_remaining
         for i in range(4):
-            yaw = np.linspace(0, t_remaining[0, i]-self.dt, int(np.floor(t_remaining[0, i]/self.dt))) * vel_cur[5, 0]
+            yaw = np.linspace(0, t_remaining[0, i]-self.dt, int(np.floor(t_remaining[0, i]/self.dt))) * vel_ref[5, 0]
             p[0, i] += (self.dt * np.cumsum(vel_cur[0, 0] * np.cos(yaw) - vel_cur[1, 0] * np.sin(yaw)))[-1]
             p[1, i] += (self.dt * np.cumsum(vel_cur[0, 0] * np.sin(yaw) + vel_cur[1, 0] * np.cos(yaw)))[-1]
 
@@ -149,7 +149,7 @@ class FootstepPlanner:
         # Add velocity forecast
         #  p += np.tile(v[0:2, 0:1], (1, 4)) * t_remaining
         for i in range(4):
-            yaw = np.linspace(0, t_remaining[0, i]-self.dt, int(np.floor(t_remaining[0, i]/self.dt))) * mpc.v[5, 0]
+            yaw = np.linspace(0, t_remaining[0, i]-self.dt, int(np.floor(t_remaining[0, i]/self.dt))) * mpc.v_ref[5, 0]
             p[0, i] += (self.dt * np.cumsum(mpc.v[0, 0] * np.cos(yaw) - mpc.v[1, 0] * np.sin(yaw)))[-1]
             p[1, i] += (self.dt * np.cumsum(mpc.v[0, 0] * np.sin(yaw) + mpc.v[1, 0] * np.cos(yaw)))[-1]
 
@@ -195,7 +195,7 @@ class FootstepPlanner:
 
         # Add velocity forecast
         for i in range(4):
-            yaw = np.linspace(0, t_remaining[0, i]-self.dt, int(np.floor(t_remaining[0, i]/self.dt))) * v[5, 0]
+            yaw = np.linspace(0, t_remaining[0, i]-self.dt, int(np.floor(t_remaining[0, i]/self.dt))) * v_ref[5, 0]
             p[0, i] += (self.dt * np.cumsum(v[0, 0] * np.cos(yaw) - v[1, 0] * np.sin(yaw)))[-1]
             p[1, i] += (self.dt * np.cumsum(v[0, 0] * np.sin(yaw) + v[1, 0] * np.cos(yaw)))[-1]
 

Joystick.py
@@ -14,18 +14,22 @@ class Joystick:
         self.t_start = clock()
 
         # Reference velocity in local frame
-        self.v_ref = np.array([[0.1, 0.0, 0.0, 0.0, 0.0, 0.2]]).T
+        self.v_ref = np.array([[0.0, 0.0, 0.0, 0.0, 0.0, 0.0]]).T
 
     def update_v_ref(self, k_loop):
 
         # Change reference velocity during the simulation (in trunk frame)
-
         # Moving forwards
-        """if k_loop == 75:
-            self.v_ref = np.array([[0.1, 0, 0.0, 0, 0, 0.0]]).T"""
+        """if k_loop == 200:
+            self.v_ref = np.array([[0.1, 0.0, 0.0, 0.0, 0.0, 0.0]]).T"""
+        """
 
         # Turning
-        """if k_loop == 151:
-            self.v_ref = np.array([[0.1, 0, 0.0, 0, 0, 0.4]]).T"""
+        if k_loop == 2500:
+            self.v_ref = np.array([[0.0, 0.0, 0.0, 0.0, 0.0, 0.2]]).T"""
+
+        # Moving forwards
+        if k_loop == 3500:
+            self.v_ref = np.array([[0.1, 0.0, 0.0, 0.0, 0.0, 0.0]]).T
 
         return 0

TSID_Debug_controller_four_legs_fb_vel.py
@@ -302,11 +302,16 @@ class controller:
         for i_foot in feet:
 
             # Get desired 3D position
-            [x0, dx0, ddx0,  y0, dy0, ddy0,  z0, dz0, ddz0, gx1, gy1] = (self.ftgs[i_foot]).get_next_foot(
+            """[x0, dx0, ddx0,  y0, dy0, ddy0,  z0, dz0, ddz0, gx1, gy1] = (self.ftgs[i_foot]).get_next_foot(
                 self.sampleFeet[i_foot].pos()[0, 0], self.sampleFeet[i_foot].vel()[
                     0, 0], self.sampleFeet[i_foot].acc()[1, 0],
                 self.sampleFeet[i_foot].pos()[1, 0], self.sampleFeet[i_foot].vel()[
                     1, 0], self.sampleFeet[i_foot].acc()[1, 0],
+                x1[i_foot], y1[i_foot], t0,  t1, dt)"""
+
+            [x0, dx0, ddx0,  y0, dy0, ddy0,  z0, dz0, ddz0, gx1, gy1] = (self.ftgs[i_foot]).get_next_foot(
+                mpc_interface.o_feet[0, i_foot], mpc_interface.ov_feet[0, i_foot], mpc_interface.oa_feet[0, i_foot],
+                mpc_interface.o_feet[1, i_foot], mpc_interface.ov_feet[1, i_foot], mpc_interface.oa_feet[0, i_foot],
                 x1[i_foot], y1[i_foot], t0,  t1, dt)
 
             # Take into account vertical offset of Pybullet
@@ -455,7 +460,7 @@ class controller:
         tmp[1, 0] = np.sum(mpc.f_applied[2::3]) / mass
         tmp[2, 0] = np.sum(mpc.f_applied[3::3]) / mass - 9.81
         self.sample_com.acc(tmp)"""
-        self.comTask.setReference(self.sample_com)
+        # self.comTask.setReference(self.sample_com)
 
         """self.sampleTrunk.pos(np.matrix([tmp[0, 0], tmp[1, 0], 0.235 - 0.01205385,
                                         1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0]).T)

main.py
@@ -22,7 +22,7 @@ dt_mpc = 0.005
 t = 0.0  # Time
 
 # Simulation parameters
-N_SIMULATION = 1500  # number of time steps simulated
+N_SIMULATION = 500  # number of time steps simulated
 
 # Initialize the error for the simulation time
 time_error = False
@@ -73,11 +73,21 @@ for k in range(int(N_SIMULATION)):
     baseState = pyb.getBasePositionAndOrientation(pyb_sim.robotId)  # Position and orientation of the trunk
     baseVel = pyb.getBaseVelocity(pyb_sim.robotId)  # Velocity of the trunk
 
+    test1 = pyb.getEulerFromQuaternion(np.array([baseState[1]]).T)
+    test1 = [test1[0], test1[1], test1[2] + 3.1415 * 0.5]
+    test2 = pyb.getQuaternionFromEuler(test1)
+
     # Joints configuration and velocity vector for free-flyer + 12 actuators
     qmes12 = np.vstack((np.array([baseState[0]]).T, np.array([baseState[1]]).T,
                         np.array([[jointStates[i_joint][0] for i_joint in range(len(jointStates))]]).T))
     vmes12 = np.vstack((np.array([baseVel[0]]).T, np.array([baseVel[1]]).T,
                         np.array([[jointStates[i_joint][1] for i_joint in range(len(jointStates))]]).T))
+    if k > 50:
+        # Joints configuration and velocity vector for free-flyer + 12 actuators
+        qmes12 = np.vstack((np.array([baseState[0]]).T, np.array([test2]).transpose(),  # np.array([baseState[1]]).T,
+                            np.array([[jointStates[i_joint][0] for i_joint in range(len(jointStates))]]).T))
+        vmes12 = np.vstack((np.array([baseVel[0]]).T, np.array([baseVel[1]]).T,
+                            np.array([[jointStates[i_joint][1] for i_joint in range(len(jointStates))]]).T))
 
     ########################
     # Update MPC interface #
@@ -201,7 +211,7 @@ for k in range(int(N_SIMULATION)):
     # Logging various stuff
     logger.call_log_functions(sequencer, fstep_planner, ftraj_gen, mpc, k)
 
-    if False:  # k in [156]:
+    if False:  # k in [228]:
         fc = mpc.x[mpc.xref.shape[0] * (mpc.xref.shape[1]-1):].reshape((12, -1), order='F')
 
         # Plot desired contact forces