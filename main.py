05e44741a02b0f870a8fd3445dbab95f9c8cceea
Pierre-Alexandre Leziart <paleziart@laas.fr>
2020-07-01 18:36:07 +0200

    Adapt main.py to be able to launch scenarios from scenarios.py

--- main.py
+++ main.py
@@ -11,109 +11,117 @@ import EmergencyStop_controller
 import ForceMonitor
 import processing as proc
 import MPC_Virtual
+import pybullet as pyb
 
-########################################################################
-#                        Parameters definition                         #
-########################################################################
 
-# Time step
-dt_mpc = 0.02
-k_mpc = int(dt_mpc / dt)  # dt is dt_tsid, defined in the TSID controller script
-t = 0.0  # Time
-n_periods = 1  # Number of periods in the prediction horizon
-T_gait = 0.48  # Duration of one gait period
-# Simulation parameters
-N_SIMULATION = 1000  # number of time steps simulated
+def run_scenario(envID, velID, dt_mpc, k_mpc, t, n_periods, T_gait, N_SIMULATION, type_MPC):
 
-# Initialize the error for the simulation time
-time_error = False
+    ########################################################################
+    #                        Parameters definition                         #
+    ########################################################################
+    """# Time step
+    dt_mpc = 0.02
+    k_mpc = int(dt_mpc / dt)  # dt is dt_tsid, defined in the TSID controller script
+    t = 0.0  # Time
+    n_periods = 1  # Number of periods in the prediction horizon
+    T_gait = 0.48  # Duration of one gait period
+    # Simulation parameters
+    N_SIMULATION = 1000  # number of time steps simulated"""
 
-# Lists to log the duration of 1 iteration of the MPC/TSID
-t_list_tsid = [0] * int(N_SIMULATION)
+    # Initialize the error for the simulation time
+    time_error = False
 
-# List to store the IDs of debug lines
-ID_deb_lines = []
+    # Lists to log the duration of 1 iteration of the MPC/TSID
+    t_list_tsid = [0] * int(N_SIMULATION)
 
-# Enable/Disable Gepetto viewer
-enable_gepetto_viewer = True
+    # List to store the IDs of debug lines
+    ID_deb_lines = []
 
-# Which MPC solver you want to use
-# True to have PA's MPC, to False to have Thomas's MPC
-type_MPC = True
+    # Enable/Disable Gepetto viewer
+    enable_gepetto_viewer = True
 
-# Create Joystick, FootstepPlanner, Logger and Interface objects
-joystick, fstep_planner, logger, interface = utils.init_objects(
-    dt, dt_mpc, N_SIMULATION, k_mpc, n_periods, T_gait, type_MPC)
+    # Which MPC solver you want to use
+    # True to have PA's MPC, to False to have Thomas's MPC
+    """type_MPC = True"""
 
-# Wrapper that makes the link with the solver that you want to use for the MPC
-# First argument to True to have PA's MPC, to False to have Thomas's MPC
-mpc_wrapper = MPC_Virtual.MPC_Virtual(type_MPC, dt_mpc, fstep_planner.n_steps, k_mpc, fstep_planner.T_gait)
+    # Create Joystick, FootstepPlanner, Logger and Interface objects
+    joystick, fstep_planner, logger, interface = utils.init_objects(
+        dt, dt_mpc, N_SIMULATION, k_mpc, n_periods, T_gait, type_MPC)
 
-# Enable/Disable hybrid control
-enable_hybrid_control = True
+    # Wrapper that makes the link with the solver that you want to use for the MPC
+    # First argument to True to have PA's MPC, to False to have Thomas's MPC
+    mpc_wrapper = MPC_Virtual.MPC_Virtual(type_MPC, dt_mpc, fstep_planner.n_steps, k_mpc, fstep_planner.T_gait)
 
-########################################################################
-#                            Gepetto viewer                            #
-########################################################################
+    # Enable/Disable hybrid control
+    enable_hybrid_control = True
 
-# Initialisation of the Gepetto viewer
-solo = utils.init_viewer(enable_gepetto_viewer)
+    ########################################################################
+    #                            Gepetto viewer                            #
+    ########################################################################
 
-########################################################################
-#                              PyBullet                                #
-########################################################################
+    # Initialisation of the Gepetto viewer
+    solo = utils.init_viewer(enable_gepetto_viewer)
 
-# Initialisation of the PyBullet simulator
-pyb_sim = utils.pybullet_simulator(dt=0.001)
+    ########################################################################
+    #                              PyBullet                                #
+    ########################################################################
 
-# Force monitor to display contact forces in PyBullet with red lines
-myForceMonitor = ForceMonitor.ForceMonitor(pyb_sim.robotId, pyb_sim.planeId)
+    # Initialisation of the PyBullet simulator
+    pyb_sim = utils.pybullet_simulator(dt=0.001)
 
-########################################################################
-#                             Simulator                                #
-########################################################################
+    # Force monitor to display contact forces in PyBullet with red lines
+    myForceMonitor = ForceMonitor.ForceMonitor(pyb_sim.robotId, pyb_sim.planeId)
 
-# Define the default controller as well as emergency and safety controller
-myController = controller(int(N_SIMULATION), k_mpc, n_periods, T_gait)
-mySafetyController = Safety_controller.controller_12dof()
-myEmergencyStop = EmergencyStop_controller.controller_12dof()
+    ########################################################################
+    #                             Simulator                                #
+    ########################################################################
 
-for k in range(int(N_SIMULATION)):
+    # Define the default controller as well as emergency and safety controller
+    myController = controller(int(N_SIMULATION), k_mpc, n_periods, T_gait)
+    mySafetyController = Safety_controller.controller_12dof()
+    myEmergencyStop = EmergencyStop_controller.controller_12dof()
 
-    if (k % 1000) == 0:
-        print("Iteration: ", k)
+    for k in range(int(N_SIMULATION)):
 
-    # Process states update and joystick
-    proc.process_states(solo, k, k_mpc, pyb_sim, interface, joystick, myController)
+        if (k % 1000) == 0:
+            print("Iteration: ", k)
 
-    # Process footstep planner
-    proc.process_footsteps_planner(k, k_mpc, pyb_sim, interface, joystick, fstep_planner)
+        # Process states update and joystick
+        proc.process_states(solo, k, k_mpc, pyb_sim, interface, joystick, myController)
 
-    # Process MPC once every k_mpc iterations of TSID
-    if (k % k_mpc) == 0:
-        f_applied = proc.process_mpc(k, k_mpc, interface, joystick, fstep_planner, mpc_wrapper,
-                                     dt_mpc, ID_deb_lines)
+        # Process footstep planner
+        proc.process_footsteps_planner(k, k_mpc, pyb_sim, interface, joystick, fstep_planner)
 
-    # Process Inverse Dynamics
-    time_tsid = time.time()
-    jointTorques = proc.process_invdyn(solo, k, f_applied, pyb_sim, interface, fstep_planner,
-                                       myController, enable_hybrid_control)
-    t_list_tsid[k] = time.time() - time_tsid  # Logging the time spent to run this iteration of inverse dynamics
+        # Process MPC once every k_mpc iterations of TSID
+        if (k % k_mpc) == 0:
+            f_applied = proc.process_mpc(k, k_mpc, interface, joystick, fstep_planner, mpc_wrapper,
+                                         dt_mpc, ID_deb_lines)
 
-    # Process PyBullet
-    proc.process_pybullet(pyb_sim, k, jointTorques)
+        # Process Inverse Dynamics
+        time_tsid = time.time()
+        jointTorques = proc.process_invdyn(solo, k, f_applied, pyb_sim, interface, fstep_planner,
+                                           myController, enable_hybrid_control)
+        t_list_tsid[k] = time.time() - time_tsid  # Logging the time spent to run this iteration of inverse dynamics
 
-    # Call logger object to log various parameters
-    logger.call_log_functions(k, pyb_sim, joystick, fstep_planner, interface, mpc_wrapper, myController,
-                              False, pyb_sim.robotId, pyb_sim.planeId, solo)
+        # Process PyBullet
+        proc.process_pybullet(pyb_sim, k, jointTorques)
 
-####################
-# END OF MAIN LOOP #
-####################
+        # Call logger object to log various parameters
+        logger.call_log_functions(k, pyb_sim, joystick, fstep_planner, interface, mpc_wrapper, myController,
+                                  False, pyb_sim.robotId, pyb_sim.planeId, solo)
 
-print("END")
+    ####################
+    # END OF MAIN LOOP #
+    ####################
 
-# Display what has been logged by the logger
+    print("END")
+
+    pyb.disconnect()
+
+    return logger
+
+
+"""# Display what has been logged by the logger
 logger.plot_graphs(enable_multiprocessing=False)
 
 quit()
@@ -122,4 +130,4 @@ quit()
 plt.figure()
 plt.plot(t_list_tsid, 'k+')
 plt.title("Time TSID")
-plt.show(block=True)
+plt.show(block=True)"""
