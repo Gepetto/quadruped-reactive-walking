Cleaning MPC + fix MPC default output (the one before the MPC finishes its computation)

scripts/MPC_Wrapper.py
@@ -83,15 +83,20 @@ class MPC_Wrapper:
         else:  # Run in the same process than main loop
             self.run_MPC_synchronous(k, fstep_planner)
 
-        if k > 2 and not np.array_equal(self.gait_memory, fstep_planner.gait[0, 1:]):
-            self.gait_memory = (fstep_planner.gait[0, 1:]).copy()
-            mass = 2.5
-            nb_ctc = np.sum(fstep_planner.gait[0, 1:])
+        if k > 2:
+            self.last_available_result[12:(12+self.n_steps), :] = np.roll(self.last_available_result[12:(12+self.n_steps), :], -1, axis=1)
+
+        pt = 0
+        while (fstep_planner.gait[pt, 0] != 0):
+            pt += 1
+        if k > 2 and not np.array_equal(fstep_planner.gait[0, 1:], fstep_planner.gait[pt-1, 1:]):
+            mass = 2.5  # Todo: grab from URDF?
+            nb_ctc = np.sum(fstep_planner.gait[pt-1, 1:])
             F = 9.81 * mass / nb_ctc
-            self.last_available_result[12:, 0] = np.zeros(12)
+            self.last_available_result[12:, self.n_steps-1] = np.zeros(12)
             for i in range(4):
-                if (fstep_planner.gait[0, 1+i] == 1):
-                    self.last_available_result[12+3*i+2, 0] = F
+                if (fstep_planner.gait[pt-1, 1+i] == 1):
+                    self.last_available_result[12+3*i+2, self.n_steps-1] = F
 
         return 0
 