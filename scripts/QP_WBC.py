02bd0c83e5d28c22963178b3b1dcc70a87556bda
paleziart <paleziart@laas.fr>
2021-06-08 11:11:10 +0200

    Add whole body control interface to new architecture

--- scripts/QP_WBC.py
+++ scripts/QP_WBC.py
@@ -6,7 +6,7 @@ from solo12InvKin import Solo12InvKin
 from time import perf_counter as clock
 from time import time
 import libquadruped_reactive_walking as lrw
-
+from example_robot_data import load
 
 class wbc_controller():
     """Whole body controller which contains an Inverse Kinematics step and a BoxQP step
@@ -16,6 +16,8 @@ class wbc_controller():
     """
 
     def __init__(self, dt, N_SIMULATION):
+
+        self.robot = load('solo12')
 
         self.dt = dt  # Time step
 
@@ -46,14 +48,13 @@ class wbc_controller():
         # Indexes of feet frames in this order: [FL, FR, HL, HR]
         self.indexes = [10, 18, 26, 34]
 
-    def compute(self, q, dq, x_cmd, f_cmd, contacts, pgoals, vgoals, agoals):
+    def compute(self, q, dq, f_cmd, contacts, pgoals, vgoals, agoals):
         """ Call Inverse Kinematics to get an acceleration command then
         solve a QP problem to get the feedforward torques
 
         Args:
             q (19x1): Current state of the base
             dq (18x1): Current velocity of the base (in base frame)
-            x_cmd (1x12): Position and velocity references from the mpc
             f_cmd (1x12): Contact forces references from the mpc
             contacts (1x4): Contact status of feet
             planner (object): Object that contains the pos, vel and acc references for feet
@@ -66,12 +67,12 @@ class wbc_controller():
         self.tic = time()
 
         # Compute Inverse Kinematics
-        ddq_cmd = np.array([self.invKin.refreshAndCompute(q.copy(), dq.copy(), x_cmd, contacts, pgoals, vgoals, agoals)]).T
+        ddq_cmd = np.array([self.invKin.refreshAndCompute(q[7:, 0:1], dq[6:, 0:1], contacts, pgoals, vgoals, agoals)]).T
 
         for i in range(4):
-            self.log_feet_pos[:, i, self.k_log] = self.invKin.rdata.oMf[self.indexes[i]].translation
-            self.log_feet_err[:, i, self.k_log] = self.invKin.feet_position_ref[i] - self.invKin.rdata.oMf[self.indexes[i]].translation # self.invKin.pfeet_err[i]
-            self.log_feet_vel[:, i, self.k_log] = pin.getFrameVelocity(self.invKin.rmodel, self.invKin.rdata,
+            self.log_feet_pos[:, i, self.k_log] = self.invKin.robot.data.oMf[self.indexes[i]].translation
+            self.log_feet_err[:, i, self.k_log] = pgoals[:, i] - self.invKin.robot.data.oMf[self.indexes[i]].translation # self.invKin.pfeet_err[i]
+            self.log_feet_vel[:, i, self.k_log] = pin.getFrameVelocity(self.invKin.robot.model, self.invKin.robot.data,
                                                                        self.indexes[i], pin.LOCAL_WORLD_ALIGNED).linear
         self.feet_pos = self.log_feet_pos[:, :, self.k_log]
         self.feet_err = self.log_feet_err[:, :, self.k_log]
@@ -80,22 +81,26 @@ class wbc_controller():
         self.log_feet_pos_target[:, :, self.k_log] = pgoals[:, :]
         self.log_feet_vel_target[:, :, self.k_log] = vgoals[:, :]
         self.log_feet_acc_target[:, :, self.k_log] = agoals[:, :]
-        
+
         self.tac = time()
 
         # Compute the joint space inertia matrix M by using the Composite Rigid Body Algorithm
-        self.M = pin.crba(self.invKin.rmodel, self.invKin.rdata, q)
+        q_tmp = np.zeros((19, 1))
+        q_tmp[6, 0] = 1.0
+        self.M = pin.crba(self.robot.model, self.robot.data, q_tmp)
+
+        self.M[:6, :6] = self.M[:6, :6] * (np.eye(6) == 1)  # (self.M[:6, :6] > 1e-3)
 
         # Compute Jacobian of contact points
-        #print("##")
+        pin.computeJointJacobians(self.robot.model, self.robot.data, q)
         self.Jc = np.zeros((12, 18))
-        for i in range(4):
-            if contacts[i]:
-                # Feet Jacobian were already retrieved in InvKin so no need to call getFrameJacobian
-                self.Jc[(3*i):(3*(i+1)), :] = (self.invKin.cpp_Jf[(3*i):(3*(i+1)), :]).copy()
+        for i_ee in range(4):
+            if contacts[i_ee]:
+                idx = int(self.invKin.foot_ids[i_ee])
+                self.Jc[(3*i_ee):(3*(i_ee+1)), :] = pin.getFrameJacobian(self.robot.model, self.robot.data, idx, pin.LOCAL_WORLD_ALIGNED)[:3]
 
         # Compute joint torques according to the current state of the system and the desired joint accelerations
-        RNEA = pin.rnea(self.invKin.rmodel, self.invKin.rdata, q, dq, ddq_cmd)[:6]
+        RNEA = pin.rnea(self.robot.model, self.robot.data, q, dq, ddq_cmd)[:6]
 
         # Solve the QP problem with C++ bindings
         self.box_qp.run(self.M, self.Jc, f_cmd.reshape((-1, 1)), RNEA.reshape((-1, 1)), self.k_since_contact)
@@ -107,7 +112,7 @@ class wbc_controller():
         ddq_with_delta[:6, 0] += deltaddq
 
         # Compute joint torques from contact forces and desired accelerations
-        RNEA_delta = pin.rnea(self.invKin.rmodel, self.invKin.rdata, q, dq, ddq_with_delta)[6:]
+        RNEA_delta = pin.rnea(self.robot.model, self.robot.data, q, dq, ddq_with_delta)[6:]
         self.tau_ff[:] = RNEA_delta - ((self.Jc[:, 6:].transpose()) @ self.f_with_delta).ravel()
 
         # Retrieve desired positions and velocities
@@ -119,5 +124,7 @@ class wbc_controller():
         """self.tic = 0.0
         self.tac = 0.0
         self.toc = 0.0"""
+
+        self.k_log += 1
 
         return 0
